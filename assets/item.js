5cbe0ef4068b8812b2bfd7ecac9c47bd2c70fb86
xkfngs <christopherstokes@gmail.com>
2017-01-19 03:26:17 -0800

    add item menu

--- assets/item.js
+++ assets/item.js
@@ -7,3 +7,19 @@ Game.Item = function(properties) {
 };
 // Make items inherit all the functionality from glyphs
 Game.Item.extend(Game.Glyph);
+
+Game.Item.prototype.describe = function() {
+    return this._name;
+};
+
+Game.Item.prototype.describeA = function(capitalize) {
+    // optional parameter to capitalize the a/an
+    var prefixes = capitalize ? ['A', 'An'] : ['a', 'an'];
+    var string = this.describe();
+    var firstLetter = string.charAt(0).toLowerCase();
+
+    // if word starts by a vowel use an, otherwise use a
+    var prefix = 'aeiou'.indexOf(firstLetter) >= 0 ? 1 : 0;
+
+    return prefixes[prefix] + ' ' + string;
+};
